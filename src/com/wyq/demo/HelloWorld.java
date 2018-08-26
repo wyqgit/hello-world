f0234d7b0048eae64017ce2603f3afc5c4e9e18c
Your Name <you@example.com>
2018-08-26 22:43:09 +0800

    update

--- src/com/wyq/demo/HelloWorld.java
+++ src/com/wyq/demo/HelloWorld.java
@@ -2,6 +2,13 @@ package com.wyq.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        print();
+        String temp = "tom";
+        System.out.println("-----------YouMeek.com-----------temp值=" + temp + "," + "当前类=HelloWorld.main()");
+
+    }
+
+    private static void print() {
         System.out.println("hello world!");
         System.out.println("git test!");
     }
