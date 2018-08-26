8c3f969f1465c3e762f6314c99aaa597a1c727de
Your Name <you@example.com>
2018-08-26 10:03:08 +0800

    add "!"

--- src/com/wyq/demo/HelloWorld.java
+++ src/com/wyq/demo/HelloWorld.java
@@ -2,7 +2,7 @@ package com.wyq.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!");
         System.out.println("git test");
     }
 }
