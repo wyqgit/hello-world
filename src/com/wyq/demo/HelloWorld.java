56083f7b61f2308fa7398a26f5c20c0aa655c9e2
wyqgit <40715042+wyqgit@users.noreply.github.com>
2018-08-26 10:06:41 +0800

    Update HelloWorld.java
    add "!" too

--- src/com/wyq/demo/HelloWorld.java
+++ src/com/wyq/demo/HelloWorld.java
@@ -3,6 +3,6 @@ package com.wyq.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("git test");
+        System.out.println("git test!");
     }
 }
