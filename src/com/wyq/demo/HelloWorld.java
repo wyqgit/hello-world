10b15f90d2ca3253e34b5b277b715ac298025f6e
Your Name <you@example.com>
2018-08-26 09:38:23 +0800

    Initial commit

---
+++ src/com/wyq/demo/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.wyq.demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("git test");
+    }
+}
